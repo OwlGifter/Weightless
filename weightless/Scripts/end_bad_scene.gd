e042e29249e314819aa3fef3afaa24964da18d46
Zabdiel Hernandez <zabdielhernandez21@gmail.com>
2025-04-27 01:48:43 -0600

    bad ending complete

--- weightless/Scripts/end_bad_scene.gd
+++ weightless/Scripts/end_bad_scene.gd
@@ -5,6 +5,10 @@ var speed = 2
 func _process(delta):
 		var new_position = $Camera2D.position + Vector2(speed, 0)
 		$Camera2D.position = new_position
-		if($Camera2D.global_position.x > 4800):
+		if($Camera2D.global_position.x > 3000):
 			speed = 0
 			pass
+
+
+func _on_end_pressed() -> void:
+	get_tree().quit()
