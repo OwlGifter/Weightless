669c00a33408a0e77432090f99582e1f61d91bc0
Zabdiel Hernandez <zabdielhernandez21@gmail.com>
2025-04-27 01:21:35 -0600

    intro done,again
    technical difficulties before

--- weightless/Scripts/intro_scene.gd
+++ weightless/Scripts/intro_scene.gd
@@ -5,10 +5,11 @@ var speed = 2
 func _process(delta):
 		var new_position = $Camera2D.position + Vector2(speed, 0)
 		$Camera2D.position = new_position
-		if($Camera2D.global_position.x > 5000):
+		if($Camera2D.global_position.x > 2900):
 			speed = 0
 			pass
 
 
-func _on_end_button_pressed() -> void:
-	get_tree().quit()
+func _on_begin_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
+	pass # Replace with function body.
