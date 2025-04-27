06461e9e81ce744654f5a62db1f00532951f91ad
FranciscoARoman <124197132+FranciscoARoman@users.noreply.github.com>
2025-04-26 23:30:22 -0600

    intro scene so far
    WIP of animation and camera panning

--- weightless/Scripts/intro_scene.gd
+++ weightless/Scripts/intro_scene.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 
 var speed = 2
+var wait_time = 2.0 # seconds to wait before moving between murals
+var timer = 0.0
+var moving = false
 
 func _process(delta):
+	if not moving:
+		timer += delta
+		if timer >= wait_time:
+			moving = true
+	else:
 		var new_position = $Camera2D.position + Vector2(speed, 0)
 		$Camera2D.position = new_position
-		if($Camera2D.global_position.x > 5000):
+		if $Camera2D.global_position.x > 0:
 			speed = 0
-			pass
