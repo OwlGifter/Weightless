4da236376306ef2df5b4a37b9fa5c30284b19709
FranciscoARoman <124197132+FranciscoARoman@users.noreply.github.com>
2025-04-27 01:11:04 -0600

    end bad scene setup
    just needs  text now

--- weightless/Scripts/end_bad_scene.gd
+++ weightless/Scripts/end_bad_scene.gd
@@ -5,6 +5,6 @@ var speed = 2
 func _process(delta):
 		var new_position = $Camera2D.position + Vector2(speed, 0)
 		$Camera2D.position = new_position
-		if($Camera2D.global_position.x > 5000):
+		if($Camera2D.global_position.x > 4800):
 			speed = 0
 			pass
--- weightless/Scripts/intro_scene.gd
+++ weightless/Scripts/intro_scene.gd
@@ -1,18 +1,14 @@
 extends Node2D
 
-
 var speed = 2
-var wait_time = 2.5 # seconds to wait before moving between murals
-var timer = 0.0
-var moving = false
 
 func _process(delta):
-	if not moving:
-		timer += delta
-		if timer >= wait_time:
-			moving = true
-	else:
 		var new_position = $Camera2D.position + Vector2(speed, 0)
 		$Camera2D.position = new_position
-		if $Camera2D.global_position.x > 1200:
+		if($Camera2D.global_position.x > 5000):
 			speed = 0
+			pass
+
+
+func _on_end_button_pressed() -> void:
+	get_tree().quit()
