1f14baae5b83a1aa9b88910ed6a14640301d2404
FranciscoARoman <124197132+FranciscoARoman@users.noreply.github.com>
2025-04-27 02:33:06 -0600

    Ammit Bite
    Added button to directly cause Ammit to bite and not loop once the animation ended

--- weightless/Scripts/game_scene.gd
+++ weightless/Scripts/game_scene.gd
@@ -1,50 +1,16 @@
 extends Node2D
 
-var goblet_cracks = 0
-var golden_goblet = 0
-var current_soul = null
 
-# @onready var soul_info = soul info
-# @onready var goblet_sprite = animation for goblet
+@onready var animation = $Ammit/AnimationPlayer
 
-func ready():
-	spawn_new_soul()
+func _process(delta):
 	pass
 
-func spawn_new_soul():
-	pass
-#current_soul = (Create soul object) Soul.new()
 
-func judge_soul(justified: bool):
-	if(current_soul.goToHeaven == justified):
-		golden_goblet += 1
-		pass
-	else:
-		goblet_cracks += 1
-		update_goblet()
-		check_game_end()
-		spawn_new_soul()
-		pass
+func _on_button_pressed() -> void:
+	animation.play("Bite Ammit")
 
-func update_goblet():
-	if goblet_cracks == 1:
-		pass
-	#goblet sprite loads the small cracked goblet
-	elif goblet_cracks == 2:
-		pass
-	#same thing but moderate cracked
-	elif goblet_cracks >= 3:
-		pass
-	# broken goblet:
-	elif golden_goblet == 1:
-		pass
-	# golden goblet starts getting gold
-	elif golden_goblet == 3:
-		pass
-	#golden goblet enters second transformation
-	elif golden_goblet == 5:
-		pass
-	#goblet turns full golden
 
-func check_game_end():
-	pass
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	
+	pass # Replace with function body.
