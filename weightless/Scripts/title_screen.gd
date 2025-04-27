659d9c00dca54d7bb8a46d84b2436d429043aed8
Zabdiel Hernandez <zabdielhernandez21@gmail.com>
2025-04-26 19:27:20 -0600

    basic scenes made
    buttons for title screen also made

---
+++ weightless/Scripts/title_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_play_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/intro_scene.tscn")
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
