974caa82c6cdeecb8484a3b698fc2d40d7602128
FranciscoARoman <124197132+FranciscoARoman@users.noreply.github.com>
2025-04-27 03:13:06 -0600

    Chompa Succ
    Successful Chompa

--- weightless/Scripts/game_scene.gd
+++ weightless/Scripts/game_scene.gd
@@ -1,12 +1,16 @@
 extends Node2D
+@onready var animation = $Ammit/AnimationPlayer
 
-var goblet_cracks = 0
-var golden_goblet = 0
-var current_soul = null
-
-# @onready var soul_info = soul info
-# @onready var goblet_sprite = animation for goblet
 
 func _process(delta: float):
 	if($music.playing == false):
 		$music.play()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	animation.play("Bite Ammit")
+	pass # Replace with function body.
